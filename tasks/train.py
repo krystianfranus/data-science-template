c8230ae2f78b6c002c422ddd962d9b994aae20f6
Krystian Franus <krystian.franus@gmail.com>
2022-07-14 23:46:08 +0200

    Improve

--- tasks/train.py
+++ tasks/train.py
@@ -30,7 +30,7 @@ def main(config: DictConfig):
         y_test = test_data["y"]
 
         # Train model
-        log.info(f"Training")
+        log.info("Training")
         n_steps = config.n_steps
         lr = config.lr
         model = LinearRegression(n_steps, lr)
