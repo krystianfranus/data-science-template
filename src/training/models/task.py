55f3648c153542011c8fdf042f5b98f7df953bff
Krystian Franus <krystian.franus@gmail.com>
2022-09-18 00:02:50 +0200

    Good Night Honey

--- src/training/models/task.py
+++ src/training/models/task.py
@@ -2,44 +2,52 @@ from typing import Any
 
 import pytorch_lightning as pl
 import torch
-
-from src.training.models.net import LinearRegressionNet
+from torchmetrics import MeanMetric
 
 
 class RegressionTask(pl.LightningModule):
     def __init__(
         self,
-        lr: float = 1e-3,
+        net: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
     ):
         super().__init__()
-        self.save_hyperparameters(logger=False)
+        self.save_hyperparameters(logger=False, ignore=["net"])
 
-        self.net = LinearRegressionNet()
+        self.net = net
         self.criterion = torch.nn.MSELoss()
+
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
 
     def step(self, batch: Any):
         x, y = batch
-        output = self.forward(x)
-        loss = self.criterion(output, y)
+        y_pred = self.forward(x)
+        loss = self.criterion(y_pred, y)
         return loss
 
     def training_step(self, batch: Any, batch_idx: int):
         loss = self.step(batch)
-        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.train_loss(loss)
+        self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
         return {"loss": loss}
 
     def validation_step(self, batch: Any, batch_idx: int):
         loss = self.step(batch)
-        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.val_loss(loss)
+        self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
         return {"loss": loss}
 
     def test_step(self, batch: Any, batch_idx: int):
         loss = self.step(batch)
-        self.log("test/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.test_loss(loss)
+        self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
         return {"loss": loss}
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.hparams["lr"])
+        optimizer = self.hparams.optimizer(params=self.parameters())
+        return optimizer
