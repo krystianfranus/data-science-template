132d7b0b30d3c58803814ebc81c160426071fa96
Krystian Franus <krystian.franus@gmail.com>
2023-03-19 17:13:01 +0100

    Fix preprocessing

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -5,172 +5,18 @@ import pandas as pd
 import polars as pl
 
 
-class MovieLens1M:
-    def __init__(self, remote_data, data_type):
-        if remote_data:
-            prefix = "s3://kfranus-bucket"
-            data_path = f"{prefix}/data-science-template/data/movielens/ratings.dat"
-        else:
-            data_path = "data/movielens/ratings.dat"
-
-        self.data = pd.read_csv(
-            data_path,
-            sep="::",
-            names=["user", "item", "target", "timestamp"],
-            engine="python",
-        )
-        self.data_type = data_type
-
-    def preprocess(self):
-        self.data = self.data.sort_values("timestamp").reset_index(drop=True)
-        self.data = self.data.drop(columns=["timestamp"])
-
-    def prepare_data(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        train_data, val_data, test_data = None, None, None
-        if self.data_type == "explicit":
-            train_data, val_data, test_data = self._prepare_explicit()
-        elif self.data_type == "implicit":
-            train_data, val_data, test_data = self._prepare_implicit()
-        elif self.data_type == "implicit_bpr":
-            train_data, val_data, test_data = self._prepare_implicit_bpr()
-        return train_data, val_data, test_data
-
-    def _prepare_explicit(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        # Split data into train/val/test
-        train_data = self.data[:900_000].reset_index(drop=True)
-        val_data = self.data[900_000:950_000].reset_index(drop=True)
-        test_data = self.data[950_000:].reset_index(drop=True)
-
-        # Prepare unique train user and items
-        train_users = train_data["user"].unique()
-        train_items = train_data["item"].unique()
-
-        # Filter val/test data
-        val_data = val_data[val_data["user"].isin(train_users)]
-        val_data = val_data[val_data["item"].isin(train_items)]
-        val_data = val_data.reset_index(drop=True)
-        test_data = test_data[test_data["user"].isin(train_users)]
-        test_data = test_data[test_data["item"].isin(train_items)]
-        test_data = test_data.reset_index(drop=True)
-
-        # Map idx
-        user_to_idx = {user: idx for idx, user in enumerate(train_users)}
-        item_to_idx = {item: idx for idx, item in enumerate(train_items)}
-        train_data["user"] = train_data["user"].map(user_to_idx)
-        train_data["item"] = train_data["item"].map(item_to_idx)
-        val_data["user"] = val_data["user"].map(user_to_idx)
-        val_data["item"] = val_data["item"].map(item_to_idx)
-        test_data["user"] = test_data["user"].map(user_to_idx)
-        test_data["item"] = test_data["item"].map(item_to_idx)
-
-        return train_data, val_data, test_data
-
-    def _prepare_implicit(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        self.data.loc[self.data["target"] < 4, "target"] = 0
-        self.data.loc[self.data["target"] >= 4, "target"] = 1
-
-        # Split data into train/val/test
-        train_data = self.data[:900_000].reset_index(drop=True)
-        val_data = self.data[900_000:950_000].reset_index(drop=True)
-        test_data = self.data[950_000:].reset_index(drop=True)
-
-        # Prepare unique train user and items
-        train_users = train_data["user"].unique()
-        train_items = train_data["item"].unique()
-
-        # Filter val/test data
-        val_data = val_data[val_data["user"].isin(train_users)]
-        val_data = val_data[val_data["item"].isin(train_items)]
-        val_data = val_data.reset_index(drop=True)
-        test_data = test_data[test_data["user"].isin(train_users)]
-        test_data = test_data[test_data["item"].isin(train_items)]
-        test_data = test_data.reset_index(drop=True)
-
-        # Map idx
-        user_to_idx = {user: idx for idx, user in enumerate(train_users)}
-        item_to_idx = {item: idx for idx, item in enumerate(train_items)}
-        train_data["user"] = train_data["user"].map(user_to_idx)
-        train_data["item"] = train_data["item"].map(item_to_idx)
-        val_data["user"] = val_data["user"].map(user_to_idx)
-        val_data["item"] = val_data["item"].map(item_to_idx)
-        test_data["user"] = test_data["user"].map(user_to_idx)
-        test_data["item"] = test_data["item"].map(item_to_idx)
-
-        return train_data, val_data, test_data
-
-    def _prepare_implicit_bpr(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        self.data.loc[self.data["target"] < 4, "target"] = 0
-        self.data.loc[self.data["target"] >= 4, "target"] = 1
-
-        # Split data into train/val/test
-        train_data = self.data[:900_000].reset_index(drop=True)
-        tmp0 = train_data.loc[train_data["target"] == 0, ["user", "item"]]
-        tmp1 = train_data.loc[train_data["target"] == 1, ["user", "item"]]
-        train_data = tmp0.merge(tmp1, "inner", "user", suffixes=("_neg", "_pos"))
-        train_data = train_data.sample(frac=0.05, random_state=0).reset_index(drop=True)
-        val_data = self.data[900_000:950_000].reset_index(drop=True)
-        test_data = self.data[950_000:].reset_index(drop=True)
-
-        # Prepare unique train user and items
-        train_users = train_data["user"].unique()
-        item_neg_set = set(train_data["item_neg"])
-        item_pos_set = set(train_data["item_pos"])
-        train_items = pd.Series(list(item_neg_set | item_pos_set)).unique()
-
-        # Filter val/test data
-        val_data = val_data[val_data["user"].isin(train_users)]
-        val_data = val_data[val_data["item"].isin(train_items)]
-        val_data = val_data.reset_index(drop=True)
-        test_data = test_data[test_data["user"].isin(train_users)]
-        test_data = test_data[test_data["item"].isin(train_items)]
-        test_data = test_data.reset_index(drop=True)
-
-        # Map idx
-        user_to_idx = {user: idx for idx, user in enumerate(train_users)}
-        item_to_idx = {item: idx for idx, item in enumerate(train_items)}
-        train_data["user"] = train_data["user"].map(user_to_idx)
-        train_data["item_neg"] = train_data["item_neg"].map(item_to_idx)
-        train_data["item_pos"] = train_data["item_pos"].map(item_to_idx)
-        val_data["user"] = val_data["user"].map(user_to_idx)
-        val_data["item"] = val_data["item"].map(item_to_idx)
-        test_data["user"] = test_data["user"].map(user_to_idx)
-        test_data["item"] = test_data["item"].map(item_to_idx)
-
-        return train_data, val_data, test_data
-
-    def save_data(self, train_data, val_data, test_data):
-        train_data.to_csv(
-            f"data/movielens/train_data_{self.data_type}.csv", index=False, header=False
-        )
-        val_data.to_csv(
-            f"data/movielens/val_data_{self.data_type}.csv", index=False, header=False
-        )
-        test_data.to_csv(
-            f"data/movielens/test_data_{self.data_type}.csv", index=False, header=False
-        )
-
-
 class ContentWise:
-    def __init__(self, remote_data, data_type):
-        if remote_data:  # TODO
-            prefix = "s3://kfranus-bucket"
-            interactions_path = (
-                f"{prefix}/data-science-template/data/movielens/ratings.dat"
-            )
-            impressions_dl_path = (
-                f"{prefix}/data-science-template/data/movielens/ratings.dat"
-            )
+    def __init__(self, data_type, use_remote_storage):
+        if use_remote_storage:
+            prefix = "s3://kf-north-bucket/data-science-template/data/contentwise/CW10M"
         else:
             prefix = "data/contentwise/data/contentwise/CW10M"
-            interactions_path = f"{prefix}/interactions/*.parquet"
-            impressions_dl_path = f"{prefix}/impressions-direct-link/*.parquet"
+        interactions_path = f"{prefix}/interactions/*.parquet"
+        impressions_dl_path = f"{prefix}/impressions-direct-link/*.parquet"
 
         self.interactions = pl.scan_parquet(interactions_path)
         self.impressions_dl = pl.scan_parquet(impressions_dl_path)
         self.data_type = data_type
-
-    def preprocess(self):
-        pass
 
     def prepare_data(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         train_data, val_data, test_data = None, None, None
--- steps/preprocess.py
+++ steps/preprocess.py
@@ -1,8 +1,9 @@
 import logging
-
 import hydra
 from clearml import Task, TaskTypes
 from omegaconf import DictConfig
+
+from src.preprocessing.preprocessing import ContentWise
 
 log = logging.getLogger(__name__)
 
@@ -10,36 +11,35 @@ log = logging.getLogger(__name__)
 @hydra.main(
     version_base=None, config_path="../configs/preprocessing/", config_name="config"
 )
-def main(config: DictConfig):
-    if config.execute_locally:
-        remote_data = False
+def main(cfg: DictConfig):
+    if cfg.use_remote_storage:
+        output_uri = "s3://kf-north-bucket/data-science-template/output/"
     else:
+        output_uri = None
+
+    if cfg.clearml:
         task = Task.init(
-            project_name="My project",
+            project_name="MyProject",
             task_name="Preprocessing",
             task_type=TaskTypes.data_processing,
-            output_uri="s3://kfranus-bucket/data-science-template/output/",
+            output_uri=output_uri,
         )
-        remote_data = True
-
-        if config.draft_mode:
+        if cfg.draft_mode:
             task.execute_remotely()
 
-    data = hydra.utils.instantiate(config.data, remote_data=remote_data)
-    log.info("[My Logger] Data loading")
-    data.preprocess()
-    log.info("[My Logger] Data parsing")
+    log.info("Data loading")
+    data = ContentWise(cfg.data_type, cfg.use_remote_storage)
+
+    log.info("Data parsing")
     train_data, val_data, test_data = data.prepare_data()
 
-    log.info("[My Logger] Data (artifacts) uploading")
-    if config.execute_locally:
-        data.save_data(train_data, val_data, test_data)
-    else:
+    log.info("Data (artifacts) saving")
+    if cfg.clearml:
         task.upload_artifact("train_data", train_data)
         task.upload_artifact("val_data", val_data)
         task.upload_artifact("test_data", test_data)
-
-    log.info("[My Logger] Done!")
+    else:
+        data.save_data(train_data, val_data, test_data)
 
 
 if __name__ == "__main__":
