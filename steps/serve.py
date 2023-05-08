bffe531021aa6a7678a0332c877fc4d7377de7a9
Krystian Franus <krystian.franus@gmail.com>
2023-05-08 21:54:33 +0200

    Improve

---
+++ steps/serve.py
@@ -0,0 +1,28 @@
+import torch
+from clearml import Task
+from flask import Flask, jsonify, request
+
+from mypackage.training.models.task import SimpleMLPTask
+
+app = Flask(__name__)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    task_prev = Task.get_task(project_name="MyProject", task_name="Training")
+    ckpt_path = task_prev.models["input"][-1].get_local_copy()
+    model = SimpleMLPTask.load_from_checkpoint(ckpt_path).cpu()
+    model.eval()
+
+    input_data = torch.tensor(request.json)
+    users, items = input_data[:, 0], input_data[:, 1]
+
+    with torch.no_grad():
+        prediction = model.predict(users, items)
+
+    response = {"prediction": prediction.tolist()}
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
